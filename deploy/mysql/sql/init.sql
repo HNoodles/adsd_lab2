9ad81e175ce38bc28344a017c27c350b964bc00d
zhaidengzhan <17302010033@fudan.edu.cn>
2022-01-07 17:47:40 +0800

    部署文件

---
+++ deploy/mysql/sql/init.sql
@@ -0,0 +1,4 @@
+create database `adsdLab-employee` default character set utf8 collate utf8_general_ci;
+create database `adsdLab-user` default character set utf8 collate utf8_general_ci;
+create database `adsdLab-report` default character set utf8 collate utf8_general_ci;
+create database `adsdLab-task` default character set utf8 collate utf8_general_ci;
